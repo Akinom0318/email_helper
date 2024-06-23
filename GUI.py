947dad0f83c3349d3065b29025d946af4d870014
Akinom <wuten770623@gmail.com>
2024-06-23 09:04:46 +0800

    try on email feature

--- GUI.py
+++ GUI.py
@@ -1,4 +1,5 @@
 import dearpygui.dearpygui as dpg
+import pyperclip as pc
 import test
 
 def check_valid():
@@ -18,19 +19,18 @@ def clear_login_page():
     dpg.delete_item("gmail_icon")
 
 def email_session():
-    global title,sender,context
-    title = dpg.add_text(test.data[1][0], tag = "title", parent = "Email helper")
-    sender = dpg.add_text(test.data[1][1], tag = "sender", parent = "Email helper")
-    context = dpg.add_text(test.data[1][2], tag = "context", parent = "Email helper", wrap = 2000)
-
+    global title,sender,context,GPT
+    title = dpg.add_text(test.data[1][0], tag = "title", parent = "main")
+    sender = dpg.add_text(test.data[1][1], tag = "sender", parent = "main")
+    context = dpg.add_text(test.data[1][2], tag = "context", parent = "main", wrap = 2000)
+    GPT = dpg.add_text("text reply~~~~~~~~~it has been attched to your clip board!",wrap = 600,
+                            tag = "GPT_reply",
+                            parent = "GPT",)
+    pc.copy(dpg.get_value("GPT_reply"))
 
 dpg.create_context()
 
-width, height, channels, data = dpg.load_image('Gmail_icon.png') # 0: width, 1: height, 2: channels, 3: data
-with dpg.texture_registry():
-    dpg.add_static_texture(width, height, data, tag="gmail_icon")
-
-with dpg.window(tag = "Email helper"):
+with dpg.window(tag = "main", label = "Email", width = 640, height = 800,no_resize=True,no_close=True,no_move=True):
     with dpg.font_registry():
         with dpg.font(r"kaiu.ttf", 16, tag="custom font"):
             dpg.add_font_range_hint(dpg.mvFontRangeHint_Chinese_Full)
@@ -45,15 +45,12 @@ with dpg.window(tag = "Email helper"):
     login_button = dpg.add_button(label = "Login",
                                     tag = "login_button",
                                     callback=(check_valid))
-    dpg.draw_image("gmail_icon", (1100, 20), (1180, 80), uv_min=(0, 0), uv_max=(1, 1))
+    
+with dpg.window(tag = "GPT", label = "GPT's action", width = 625, height = 800, pos = (640,0), no_resize=True, no_close=True,no_move=True):
+    pass
 
-
-
-
-dpg.create_viewport(title='Email helper', width=1280, height=800,resizable=False)
+dpg.create_viewport(title='Email Helper', width=1280, height=800,resizable=False)
 dpg.setup_dearpygui()
 dpg.show_viewport()
-dpg.set_primary_window("Email helper", True)
 dpg.start_dearpygui()
-
 dpg.destroy_context()
